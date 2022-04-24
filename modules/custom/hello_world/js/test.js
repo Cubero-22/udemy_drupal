9a4f2dc9551cfe0143a8b6b135b52cbe7ac66ebb
Cubero-22 <104138902+Cubero-22@users.noreply.github.com>
2022-04-24 10:31:54 +0200

    Se agrega el proyecto

---
+++ modules/custom/hello_world/js/test.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+  alert('Funciona este js a nivel módulo');
+});
---
+++ themes/custom/bartik_second_version/js/global.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function ($) {
+  alert('Javascript lanzado a nivel global, para todo del sitio');
+});
